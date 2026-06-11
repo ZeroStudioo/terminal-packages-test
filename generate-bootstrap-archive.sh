0d07191c7e131f72b589629a32552cab2ed7cf98
MutwakilX <144057106+Wadamzmail@users.noreply.github.com>
2026-06-11 18:13:48 +0200

    Add patches for bootstrap archive generation
    Added a series of patches to enhance the bootstrap generation process, including updates for dependencies, cleanup scripts, and configuration adjustments.

--- generate-bootstrap-archive.sh
+++ generate-bootstrap-archive.sh
@@ -11,6 +11,83 @@ script_dir=$(dirname "$script")
 COTG_RELEASE="false"
 COTG_LOCAL="false"
 
+declare -a PATCHES=(
+
+  # Adds our own GPG keys
+  "termux-keyring.patch"
+
+  # Update mirror configurations
+  "termux-tools-mirrors.patch"
+
+  # Update motd
+  "termux-tools-motd.patch"
+
+  # Makes some of the packages depend on and link against libandroid-shmem.so
+  # Required to fix some build failures
+  "libdb-depend-on-android-shmem.patch"
+  "libunbound-depend-on-android-shmem.patch"
+  "libx11-depend-on-android-shmem.patch"
+
+  # Fix dependencies in binutils-libs
+  "binutils-libs-fix-dependencies.patch"
+
+  # libxml2 v2.14.4 has build errors
+  # "libxml2-revert-to-2.14.3.patch"
+
+  # Remove 'scalar' binary from $PREFIX/bin and make it a symlink
+  # to $PREFIX/libexec/git-core/scalar
+  "git-symlink-scalar.patch"
+
+  # subversion fails to compile, complaining that the `apr.h` and other headers
+  # could not be found. These headers are located in $PREFIX/include/apr-1
+  "subversion-missing-apr-includes.patch"
+
+  # libuv has missing sources in their Makefile configuration
+  # This missing source issue was fixed in their CMake configuration
+  # So we force termux-packages to build using CMake instead of Makefile
+  "libuv-force-cmake-build.patch"
+
+  # Changes for our version of bootstrap-*.zip files
+  # This also handles the process of creating a brotli archive
+  # from the generated ZIP archive
+  "scripts-generate-bootstraps-CoGo-changes.patch"
+
+  # Update package name in termux-tools
+  "termux-tools-update-package-name.patch"
+
+  # Cleanup OpenJDK 21 to remove postinst & prerm scripts
+  "openjdk-21-cleanup.patch"
+
+  # Cleanup vim to remove postinst scripts
+  "vim-cleanup.patch"
+
+  # Restore files and cleanup in second stage
+  "scripts-cleanup-in-second-stage.patch"
+
+  # Link pulseaudio against libiconv to resolve linker errors at build time
+  "pulseaudio-link-against-libiconv.patch"
+
+  # `rm` command complains about missing libacl.so after updating packages
+  # This ensures that libacl package is installed before coreutils
+  "coreutils-depend-on-libacl.patch"
+
+  # `libapr-1.so` needs to be linked against libandroid-shmem.so
+  # in order to fix undefined symbol error when building subversion
+  "apr-link-against-libandroid-shmem.patch"
+
+  # v0.7 of the libandroid-shmem library patches shmem.c to use ASharedMemory_* from libandroid.so
+  # to work with shared memory regions. However, libandroid.so pulls in a bunch of other system
+  # libraries, including libperfetto_framework_jni.so, which assumes an ART environment and tries
+  # to register native methods of classes. This makes our tooling API server, running in a JVM env,
+  # crash due to the obviously missing Android-specific classes.
+  "libandroid-shmem-revert-a-shared-memory-patch.patch"
+
+  # termux-packages/build-package.sh contains hardcoded names for keyring files
+  # these keyring files are used when we build packages with the -I flag
+  # since we use a different signing key, we need to update the reference here
+  "use-our-keys-to-install-deps.patch"
+)
+
 usage() {
   echo "Script to generate bootstrap archives for Code On the Go."
   echo
@@ -25,6 +102,22 @@ usage() {
   echo "            Defaults to '${COTG_REPO}'."
   echo
   echo "  -h        Show this help message and exit."
+}
+
+apply_patches() {
+  pushd "$TERMUX_PACKAGES_DIR" || scribe_error_exit "Unable to pushd"
+
+  for patch in "${PATCHES[@]}"; do
+    scribe_info "Applying patch: $patch"
+
+    if patch -p1 --no-backup-if-mismatch < "$script_dir/patches/$patch"; then
+      scribe_ok "Applied $patch"
+    else
+      scribe_error_exit "Failed patch: $patch"
+    fi
+  done
+
+  popd || scribe_error_exit "Unable to popd"
 }
 
 build_boostrap() {
@@ -125,6 +218,8 @@ echo "Using configuration:"
 echo "  Variant        : ${COTG_VARIANT}"
 echo "  Repository     : ${COTG_REPO}"
 echo "  Extra packages : ${COTG_EXTRA_PACKAGES[@]}"
+
+apply_patches
 
 for arch in aarch64 arm; do
   build_boostrap "$COTG_VARIANT" "$arch" "$COTG_REPO" "${COTG_EXTRA_PACKAGES[@]}" ||
